49db78e942e1fc1f3e0ce57ae22cd442107640e2
Кирилл Яснов <kirillasnov@MacBook-Air-Kirill.local>
2022-09-19 12:45:41 +0500

    making authorization

--- backend/foodgram/food_app/views.py
+++ backend/foodgram/food_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Recipe
+from django.views.generic import ListView
 
-# Create your views here.
+
+class Index(ListView):
+    queryset = Recipe.objects.all()
+    context_object_name = 'recipes_list'
+    template_name = 'recipes/index.html'
