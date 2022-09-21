90dd73478bc6eac8cfe9d0e7ac81beb3217873c6
Кирилл Яснов <kirillasnov@MacBook-Air-Kirill.local>
2022-09-21 22:14:26 +0500

    added model Subscribe, fixed User model

--- backend/foodgram/users/models.py
+++ backend/foodgram/users/models.py
@@ -3,26 +3,61 @@ from django.db import models
 
 
 class User(AbstractUser):
-    username = models.CharField(max_length=50,
+    username = models.CharField(max_length=150,
                                 verbose_name='Имя пользователя',
-                                unique=True)
-    email = models.EmailField(max_length=250,
+                                unique=True,
+                                blank=False
+                                )
+    email = models.EmailField(max_length=254,
                               verbose_name='Электронная почта',
-                              unique=True)
-    first_name = models.CharField(max_length=50,
-                                  verbose_name='Имя')
-    last_name = models.CharField(max_length=50,
-                                 verbose_name='Фамилия')
-    bio = models.TextField(max_length=500,
-                           verbose_name='О себе',
-                           blank=True,
-                           null=True)
+                              unique=True,
+                              blank=False
+                              )
+    first_name = models.CharField(max_length=150,
+                                  verbose_name='Имя',
+                                  blank=False
+                                  )
+    last_name = models.CharField(max_length=150,
+                                 verbose_name='Фамилия',
+                                 blank=False
+                                 )
     is_staff = models.BooleanField(default=False,
-                                   verbose_name='Статус админа')
+                                   verbose_name='Статус админа'
+                                   )
 
-    REQUIRED_FIELDS = ['password', 'email',
-                       'first_name', 'last_name']
+    USERNAME_FIELD = 'email'
+
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
 
     class Meta:
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
+
+    def set_to_admin(self):
+        self.is_staff = True
+
+    def __str__(self):
+        return self.username
+
+
+class Subscribe(models.Model):
+    follower = models.ForeignKey(User,
+                                 on_delete=models.CASCADE,
+                                 related_name='following',
+                                 verbose_name='Подписчик')
+    author = models.ForeignKey(User,
+                               on_delete=models.CASCADE,
+                               related_name='subscribes',
+                               verbose_name='Автор')
+
+    class Meta:
+        verbose_name = 'Подписка'
+        verbose_name_plural = 'Подписки'
+
+        constraints = [models.UniqueConstraint(fields=['follower', 'author'],
+                                               name='unique_subscribe'
+                                               )
+                       ]
+
+    def __str__(self):
+        return f'Подписка на {self.author}'
