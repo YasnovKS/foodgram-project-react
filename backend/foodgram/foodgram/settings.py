baefc6e6731db970c68e03ed035037b5f15663e8
Кирилл Яснов <kirillasnov@MacBook-Air-Kirill.local>
2022-10-10 12:30:01 +0500

    testing deploy

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['51.250.71.61']
+ALLOWED_HOSTS = ['51.250.71.61', 'localhost', 'backend']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
