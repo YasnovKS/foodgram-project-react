a0aeaf665061a9e354d153cebf744d41fcfe7702
Кирилл Яснов <kirillasnov@MacBook-Air-Kirill.local>
2022-10-11 21:22:00 +0500

    testing recipe creating

--- backend/foodgram/food_api/filters.py
+++ backend/foodgram/food_api/filters.py
@@ -1,6 +1,6 @@
 from django_filters import rest_framework as filter
 
-from food_app.models import Ingredient, Recipe
+from food_app.models import Ingredient, Recipe, Tag
 
 
 class IngredientFilter(filter.FilterSet):
@@ -12,9 +12,30 @@ class IngredientFilter(filter.FilterSet):
 
 
 class RecipeFilter(filter.FilterSet):
-    author = filter.CharFilter(field_name='author__username')
-    tags = filter.AllValuesMultipleFilter(field_name='tags__slug')
+    tags = filter.ModelMultipleChoiceFilter(
+        field_name='tags__slug',
+        queryset=Tag.objects.all(),
+        to_field_name='slug',
+    )
+    is_favorited = filter.BooleanFilter(
+        method='get_is_favorited'
+    )
+    is_in_shopping_cart = filter.BooleanFilter(
+        method='get_is_in_shopping_cart'
+    )
 
     class Meta:
         model = Recipe
-        fields = ('tags', 'author')
+        fields = ('tags', 'author', 'is_favorited', 'is_in_shopping_cart')
+
+    def get_is_favorited(self, queryset, name, value):
+        user = self.request.user
+        if value:
+            return queryset.filter(favorites__user=user)
+        return Recipe.objects.all()
+
+    def get_is_in_shopping_cart(self, queryset, name, value):
+        user = self.request.user
+        if value:
+            return queryset.filter(purchases__user=user)
+        return Recipe.objects.all()
