02c1de134198301bee63aecbc876cac2c10aca7b
Кирилл Яснов <kirillasnov@MacBook-Air-Kirill.local>
2022-10-11 20:58:35 +0500

    testing recipe creating

--- backend/foodgram/food_api/views.py
+++ backend/foodgram/food_api/views.py
@@ -6,7 +6,7 @@ from rest_framework import (filters, generics, pagination, permissions,
                             serializers, status, views, viewsets)
 from rest_framework.response import Response
 
-from .filters import RecipeFilter
+from .filters import IngredientFilter, RecipeFilter
 from .mixins import GetObjectsViewSet, ListDetailViewSet
 from .permissions import EditPermission
 from .serializers import (GetRecipeSerializer, IngredientSerializer,
@@ -127,7 +127,7 @@ class IngredientsViewSet(GetObjectsViewSet):
     queryset = Ingredient.objects.all()
     permission_classes = [permissions.AllowAny, ]
     serializer_class = IngredientSerializer
-    search_fields = ['^name', ]
+    filterset_class = IngredientFilter
     pagination_class = None
 
 
--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -122,6 +122,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'users.User'
 
 REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': (
+        'django_filters.rest_framework.DjangoFilterBackend',
+    ),
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
