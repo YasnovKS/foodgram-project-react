cf00782b74ebf7f410952d38ab3c07a8fd7e4564
Кирилл Яснов <kirillasnov@MacBook-Air-Kirill.local>
2022-10-12 12:25:23 +0500

    chenged permissions

--- backend/foodgram/food_api/permissions.py
+++ backend/foodgram/food_api/permissions.py
@@ -13,8 +13,6 @@ class EditPermission(permissions.BasePermission):
 
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
-    def has_permission(self, request, view):
-        return request.method in permissions.SAFE_METHODS
 
     def has_object_permission(self, request, view, obj):
         return (request.method in permissions.SAFE_METHODS
